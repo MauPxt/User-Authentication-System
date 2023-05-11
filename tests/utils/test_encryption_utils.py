9ba4a804ffc1aede418ae2e43a2ed4f6290b0182
Mauricio Peixoto <90274764+MauPxt@users.noreply.github.com>
2023-05-11 02:15:49 -0300

    feat: create encryption tests

---
+++ tests/utils/test_encryption_utils.py
@@ -0,0 +1,24 @@
+import pytest
+import bcrypt
+from src.utils.encryption_utils import encrypt_password, decrypt_password
+
+
+class TestEncryptionUtils:
+    def test_encrypt_password(self):
+        password = 'password123'
+        encrypted_password = encrypt_password(password)
+
+        assert bcrypt.checkpw(
+            password.encode('utf-8'), encrypted_password.encode('utf-8')
+        )
+
+    def test_decrypt_password(self):
+        password = 'password123'
+        encrypted_password = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt()
+        )
+
+        assert (
+            decrypt_password(encrypted_password.decode('utf-8'), password)
+            is True
+        )
