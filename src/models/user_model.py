5733cc4fe1979f742421df864666f3c1a517209d
Mauricio Peixoto <90274764+MauPxt@users.noreply.github.com>
2023-05-12 22:53:27 -0300

    refactor: fix user model
    validate entries;
    add encrypt on pass;
    turn username unique on db;
    treatment and rollback.

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,7 +1,9 @@
+import re
 from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import inspect, exc
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import inspect
+from src.utils.encryption_utils import encrypt_password
 
 Base = declarative_base()
 engine = create_engine('sqlite:///database.db')
@@ -10,38 +12,75 @@ Session = sessionmaker(bind=engine)
 
 class User(Base):
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String, nullable=False)
+    username = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
 
 
 class UserDAO:
     def __init__(self):
         self.session = Session()
-
-        if not inspect(engine).has_table('users'):
-            Base.metadata.create_all(engine)
+        Base.metadata.create_all(engine, checkfirst=True)
 
     def create_user(self, username, password):
-        user = User(username=username, password=password)
-        self.session.add(user)
-        self.session.commit()
-        return user.id
+        if not self._is_valid_input(username, password):
+            return None
+
+        encrypted_password = encrypt_password(password)
+
+        try:
+            user = User(username=username, password=encrypted_password)
+            self.session.add(user)
+            self.session.commit()
+            return user.id
+        except exc.SQLAlchemyError as e:
+            self.session.rollback()
+            print(f'Error creating user: {e}')
+            return None
 
     def get_user_by_username(self, username):
-        user = self.session.query(User).filter_by(username=username).first()
-        return user
+        if not self._is_valid_input(username):
+            return None
+
+        return (
+            self.session.query(User).filter(User.username == username).first()
+        )
 
     def delete_user(self, username):
-        result = self.session.query(User).filter_by(username=username).delete()
-        self.session.commit()
-        return result == 1
+        if not self._is_valid_input(username):
+            return None
+
+        try:
+            user = (
+                self.session.query(User)
+                .filter(User.username == username)
+                .first()
+            )
+            if user:
+                self.session.delete(user)
+                self.session.commit()
+                return True
+            else:
+                return False
+        except exc.SQLAlchemyError as e:
+            self.session.rollback()
+            print(f'Error deleting user: {e}')
+            return False
+
+    def _is_valid_input(self, username=None, password=None):
+        pattern = re.compile(r'^[a-zA-Z0-9_]{8,20}$')
+        if username is not None and (
+            not username or not pattern.match(username)
+        ):
+            return False
+        if password is not None and (
+            not password or not pattern.match(password)
+        ):
+            return False
+        return True
 
 
 if __name__ == '__main__':
-    if not inspect(engine).has_table('users'):
-        Base.metadata.create_all(engine)
-
+    Base.metadata.create_all(engine, checkfirst=True)
     # user_dao = UserDAO()
     # user_dao.delete_user('test_user')
